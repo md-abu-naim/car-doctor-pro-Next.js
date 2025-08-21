d5c43ede8399da59ce7301d83eed24672546a6b3
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-21 17:27:53 +0600

    navber > logo > navlinks

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Navber = () => {
+
+    const navMenu = <>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/servicess'>Servicess</Link></li>
+        <li><Link href='/blog'>Blog</Link></li>
+        <li><Link href='/contact'>Contact</Link></li>
+    </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar text-black shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -10,32 +20,16 @@ const Navber = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navMenu}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <Link href='/' className="btn btn-ghost text-xl">
+                    <Image src='/assets/logo.svg' alt="" width={107} height={87} />
+                </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                   {navMenu }
                 </ul>
             </div>
             <div className="navbar-end">
--- src/app/layout.js
+++ src/app/layout.js
@@ -19,9 +19,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="white">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
         <Navber />
         {children}
