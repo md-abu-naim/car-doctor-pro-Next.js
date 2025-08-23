cfaf0b213ec494453fefb9ce418cefbee3bf1983
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-23 10:08:31 +0600

    mongoDB connecting

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,18 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const dbConnect = ({collectionName}) => {
+    const uri = `mongodb+srv://${process.env.MONOGDB_USER}:${process.env.MONGODB_PASS}@cluster0.zyfftle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    console.log(uri);
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    return client.db('car_doctor_pro').collection(collectionName)
+};
+
+export default dbConnect;
