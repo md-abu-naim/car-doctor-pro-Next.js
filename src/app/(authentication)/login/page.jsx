1bd2bba16cec662e3e9930b96b74027fb8f8d0ea
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-26 12:42:14 +0600

    google and github btn

--- src/app/(authentication)/login/page.jsx
+++ src/app/(authentication)/login/page.jsx
@@ -1,4 +1,5 @@
 import LoginForm from "./LoginForm";
+import SocialLogin from "./SocialLogin";
 
 const Login = () => {
   return (
@@ -17,31 +18,7 @@ const Login = () => {
         </div>
 
         {/* Social Login Buttons */}
-        <div className="space-y-3">
-          <button
-            type="button"
-            className="w-full flex items-center justify-center gap-2 bg-red-500 text-white py-2 rounded-lg hover:bg-red-600 transition"
-          >
-            <img
-              src="https://www.svgrepo.com/show/355037/google.svg"
-              alt="Google"
-              className="w-5 h-5"
-            />
-            Continue with Google
-          </button>
-
-          <button
-            type="button"
-            className="w-full flex items-center justify-center gap-2 bg-gray-800 text-white py-2 rounded-lg hover:bg-black transition"
-          >
-            <img
-              src="https://www.svgrepo.com/show/512317/github-142.svg"
-              alt="GitHub"
-              className="w-5 h-5"
-            />
-            Continue with GitHub
-          </button>
-        </div>
+        <SocialLogin />
 
         <p className="mt-6 text-center text-sm">
           Don’t have an account?{" "}
