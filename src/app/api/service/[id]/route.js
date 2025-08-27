811795d23750a5cc75ebd53c3bc80c701ae3a5b8
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-27 09:55:51 +0600

    get singleData

---
+++ src/app/api/service/[id]/route.js
@@ -0,0 +1,10 @@
+import dbConnect, { collectionNames } from "@/lib/dbConnect"
+import { ObjectId } from "mongodb"
+import { NextResponse } from "next/server"
+
+export const GET = async(req, {params}) => {
+    const id = await params
+    const service = dbConnect(collectionNames.servicess)
+    const result = await service.findOne({_id: new ObjectId(id.id)})
+    return NextResponse.json(result)
+}
--- src/app/servicess/[id]/page.jsx
+++ src/app/servicess/[id]/page.jsx
@@ -6,8 +6,11 @@ import Link from "next/link";
 const ServicessDetails = async ({ params }) => {
     const id = await params?.id
 
-    const servicessSection = dbConnect(collectionNames.servicess)
-    const service = await servicessSection.findOne({ _id: new ObjectId(id) })
+    // const servicessSection = dbConnect(collectionNames.servicess)
+    // const service = await servicessSection.findOne({ _id: new ObjectId(id) })
+
+    const res = await fetch(`http://localhost:3000/api/service/${id}`)
+    const service = await res.json() 
     return (
         <div className="min-h-screen bg-gray-50 py-10 px-4 flex justify-center">
             <div className="max-w-4xl w-full bg-white shadow-lg rounded-2xl overflow-hidden">
@@ -15,8 +18,8 @@ const ServicessDetails = async ({ params }) => {
                 <Image
                     src={service.img}
                     alt={service.title}
-                    className="w-full h-72 object-cover"
-                    width={500} height={300}
+                    className=" object-cover"
+                    width={700} height={300}
                 />
 
                 {/* Body */}
@@ -39,7 +42,7 @@ const ServicessDetails = async ({ params }) => {
                         Our Facilities
                     </h2>
                     <ul className="grid gap-4 md:grid-cols-2">
-                        {service.facility.map((f, i) => (
+                        {service?.facility?.map((f, i) => (
                             <li
                                 key={i}
                                 className="p-4 border rounded-lg bg-gray-50 hover:bg-gray-100 transition"
