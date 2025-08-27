80928d3b8fcfd694bccb8d0b05a87a3cd6e77b91
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-27 10:01:15 +0600

    checout page single Data fetch

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Checkout = async({params}) => {
-    const id = await params?
+    const id = await params?.id
+    const res = await fetch(`http://localhost:3000/api/service/${id}`)
+    const service = await res.json()
     return (
         <div>
             checkout {service.title}
